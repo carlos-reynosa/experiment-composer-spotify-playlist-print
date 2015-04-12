1e92a27590643c686c9b8468c0666ec2bd61e15c
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2015-04-12 14:28:23 -0700

    Created spotify playlist printer

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 #About
 
 Composer project to test creating and using a composer package.
+
+#Example Use
+
+```php
+$printer = new SpotifyPlaylistPrint('436a7dc3226047b39c097fb3bd8c1886','97e4536b48e6402d9182a5a4251acdd0');
+
+$printer->printList('raveash','1LnGfWZxpGpKqKeThyYLfT');
+
+var_dump($printer->getPlaylistList('raveash','1LnGfWZxpGpKqKeThyYLfT'));
+```
---
+++ src/SpotifyPlaylistPrint.php
@@ -0,0 +1,55 @@
+<?php
+namespace CarlosReynosa;
+require '../vendor/autoload.php';
+
+class SpotifyPlaylistPrint{
+
+    private $clientId = NULL;
+    private $clientSecret = NULL;
+    private $scopes = NULL;
+
+    private $spotify = NULL;
+
+    private $session = NULL;
+
+    public function __construct($clientId, $clientSecret)
+    {
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+
+        $this->scopes= array('playlist-read-private','user-read-private');
+        $this->session= new \SpotifyWebAPI\Session($clientId,$clientSecret);
+        $this->spotify= new \SpotifyWebAPI\SpotifyWebAPI();
+        $this->session->requestCredentialsToken($this->scopes);
+        $this->token=$this->session->getAccessToken();
+        $this->spotify->setAccessToken($this->token);
+
+    }
+
+    public function printList($userId,$playlistId)
+    {
+        $playlist= $this->spotify->getUserPlaylist($userId,$playlistId);
+
+        print("Playlist Name: ".$playlist->name."<br>");
+
+        foreach($playlist->tracks->items as $item ) {
+
+            print("\t- ".$item->track->name."<br>");
+        }
+    }
+
+    public function getPlaylistList($userId,$playlistId){
+
+        $playlist= $this->spotify->getUserPlaylist($userId,$playlistId);
+        $list=[];
+        foreach($playlist->tracks->items as $item ) {
+            $list[]=$item->track->name;
+        }
+
+        return $list;
+    }
+}
+
+
+
+
